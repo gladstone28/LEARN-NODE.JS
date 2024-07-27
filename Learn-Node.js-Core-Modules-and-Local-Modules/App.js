59340a60eab4e4da492027c924892b512e523061
Gladstone Rose <gladstonerose@hotmail.com>
2024-07-26 21:48:02 -0500

    modify APP.js

--- Learn-Node.js-Core-Modules-and-Local-Modules/App.js
+++ Learn-Node.js-Core-Modules-and-Local-Modules/App.js
@@ -1,6 +1,22 @@
-module.exports = class Cat {
-  constructor(name, clawStrength) {
-    this.name = name;
-    this.clawStrength = clawStrength;
-  }
-};
+// Require modules in:
+let Dog = require('./dog.js');
+let Cat = require('./cat.js'); 
+
+
+let fight = (dog, cat) => {
+    if (dog.toothStrength > cat.clawStrength) {
+        console.log(`${dog.name} wins!`);
+    }
+    else if (dog.toothStrength < cat.clawStrength) {
+        console.log(`${cat.name} wins!`);
+    }
+    else {
+        console.log(`${dog.name} and ${cat.name} are equally skilled fighters!`);
+
+    }
+}
+
+const myDog = new Dog('Rex', Math.random());
+const myCat = new Cat('Tabby', Math.random());
+
+fight(myDog, myCat);
